87e1a634c379b5e4adc3e85918452c503a7b9af3
Nadhiya37 <nadhiyahussain07@gmail.com>
2025-07-14 20:21:23 +0530

    committed

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -5,6 +5,7 @@ const AddTodo = () => {
   return (
     <div>
         <TodoNavbar/>
+        <h1 className="text-center">Add Todo</h1>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
--- src/components/DeleteTodo.jsx
+++ src/components/DeleteTodo.jsx
@@ -5,6 +5,7 @@ const DeleteTodo = () => {
   return (
     <div>
         <TodoNavbar/>
+        <h1 className="text-center">Delete Todo</h1>
          <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
